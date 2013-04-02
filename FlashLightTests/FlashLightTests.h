b8c965fa993d99d991115b31db4e95fdd8354f24
Mars182838 <wj182838@126.com>
2013-04-02 14:16:55 +0800

    Initial Commit

---
+++ FlashLightTests/FlashLightTests.h
@@ -0,0 +1,13 @@
+//
+//  FlashLightTests.h
+//  FlashLightTests
+//
+//  Created by Mars on 13-4-2.
+//  Copyright (c) 2013年 Mars. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FlashLightTests : SenTestCase
+
+@end
---
+++ FlashLightTests/FlashLightTests.m
@@ -0,0 +1,32 @@
+//
+//  FlashLightTests.m
+//  FlashLightTests
+//
+//  Created by Mars on 13-4-2.
+//  Copyright (c) 2013年 Mars. All rights reserved.
+//
+
+#import "FlashLightTests.h"
+
+@implementation FlashLightTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FlashLightTests");
+}
+
+@end
